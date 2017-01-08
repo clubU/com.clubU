e3a2c3f3f513b930205fcbf0339a66a9e3cd37dc
Corey Yen <corey.yen@mail.utoronto.ca>
2017-01-08 15:15:16 -0500

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,92 @@
+var hostname = "localhost:8080";
+
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Go to signup page
+  $scope.goToSignUp = function() {
+    $scope.modal.hide();
+    $state.go('app.signup');
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+
+    $state.go('app.user');
+  };
+})
+
+.controller('signUpCtrl', function($scope, $state, $http) {
+  //$scope.userData = {};
+  $scope.doSignUp = function (form) {
+   var request = $http({
+      method : 'POST',
+      url : hostname + 'user',
+      data : {password: $scope.password, username: $scope.username, firstName: $scope.firstName, lastName: $scope.lastName, dateOfBirth: $scope.dateOfBirth, studentNumber: $scope.studentNumber},
+      header : {}
+    });
+    $state.go('app.user');
+    request.success(function(data){
+      if (data == '1'){
+        alert("congrats");
+      }
+      else {
+        alert("you moron");
+      }
+    })
+
+  };
+
+})
+
+.controller('ClubsCtrl', function($scope) {
+  $scope.clubs = [
+    { title: 'ROCSAUT', id: 1 },
+    { title: '881', id: 2 },
+    { title: 'ICU', id: 3 },
+    { title: 'EngSoc', id: 4 }
+  ];
+})
+
+.controller('UserCtrl', function($scope) {
+  alert("userCTRL");
+})
+
+.controller('ClubCtrl', function($scope, $stateParams) {
+  alert("helloooo");
+});
