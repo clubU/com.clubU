32e66d33b9988bf008d0b30b959b268ac8d5c13e
Corey Yen <corey.yen@mail.utoronto.ca>
2017-01-13 17:26:35 -0500

    changed appearance

--- www/js/app.js
+++ www/js/app.js
@@ -32,6 +32,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
+  .state('app.start', {
+    url: '/start',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/start.html'
+      }
+    }
+  })
+
   .state('app.search', {
     url: '/search',
     views: {
@@ -117,5 +126,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/login');
+  $urlRouterProvider.otherwise('/app/start');
 });
