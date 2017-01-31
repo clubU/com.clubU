013540b3f7736b3c649dc6fc2888bf6434249920
David Shuo Chou <davidshuo.chou@mail.utoronto.ca>
2017-01-31 18:02:53 -0500

    Added Subscription API

--- backend/reset.sql
+++ backend/reset.sql
@@ -15,13 +15,13 @@ INSERT INTO club VALUES(1, "club1", "1", "Club One", "club1@club.ca", "C1", "Hel
 INSERT INTO club VALUES(2, "club2", "2", "Club Two", "club2@club.ca", "C2", "Hello, this is Club Two!", @now, @now);
 INSERT INTO club VALUES(3, "club3", "3", "Club Three", "club3@club.ca", "C3", "Hello, this is Club Three!", @now, @now);
 
-INSERT INTO subscription VALUES(1, 1, @now);
-INSERT INTO subscription VALUES(1, 3, @now);
-INSERT INTO subscription VALUES(2, 1, @now);
-INSERT INTO subscription VALUES(3, 2, @now);
-INSERT INTO subscription VALUES(4, 1, @now);
-INSERT INTO subscription VALUES(4, 2, @now);
-INSERT INTO subscription VALUES(4, 3, @now);
+INSERT INTO subscription VALUES(1, 1);
+INSERT INTO subscription VALUES(1, 3);
+INSERT INTO subscription VALUES(2, 1);
+INSERT INTO subscription VALUES(3, 2);
+INSERT INTO subscription VALUES(4, 1);
+INSERT INTO subscription VALUES(4, 2);
+INSERT INTO subscription VALUES(4, 3);
 
 INSERT INTO event VALUES(1, 1, "OTP", @now, "Somewhere", "Something", @now, @now);
 INSERT INTO event VALUES(2, 1, "LOL", @now, "Nowhere", "Nothing", @now, @now);
